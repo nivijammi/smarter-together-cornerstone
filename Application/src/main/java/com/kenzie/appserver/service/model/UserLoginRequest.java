1905aead571628c46126e329770323007d73d419
Nivi Jammi <nivedita.jammi@gmail.com>
2023-06-15 20:44:58 -0400

    UserLofInRequestAndResponse

---
+++ Application/src/main/java/com/kenzie/appserver/service/model/UserLoginRequest.java
@@ -0,0 +1,33 @@
+package com.kenzie.appserver.service.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.validation.constraints.NotEmpty;
+
+public class UserLoginRequest {
+
+    @NotEmpty
+    @JsonProperty("username")
+    private String username;
+
+    @JsonProperty("password")
+    private String password;
+
+    // getter and setters
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ Application/src/main/java/com/kenzie/appserver/service/model/UserLoginResponse.java
@@ -0,0 +1,40 @@
+package com.kenzie.appserver.service.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserLoginResponse {
+
+    @JsonProperty("userId")
+    private String userId;
+    @JsonProperty("username")
+    private String username;
+
+    @JsonProperty("token")
+    private String token;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
