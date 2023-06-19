440904e0b48d659d9e00efdca83db20c5cc3b584
Nivi Jammi <nivedita.jammi@gmail.com>
2023-06-19 12:34:05 -0400

    modified the User in controller to test controller

--- Application/src/main/java/com/kenzie/appserver/controller/model/User.java
+++ Application/src/main/java/com/kenzie/appserver/controller/model/User.java
@@ -15,5 +15,35 @@ public class User {
     @JsonProperty("userId")
     public String userId;
 
+    public String getEmail() {
+        return email;
+    }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 }
